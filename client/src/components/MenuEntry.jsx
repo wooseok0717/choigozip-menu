d9868569b9c3113ced1b9a7213950d54c0340d8c
Josh <wjang717@gmail.com>
2024-07-08 02:18:38 -0700

    adding more pictures for the menu

--- client/src/components/MenuEntry.jsx
+++ client/src/components/MenuEntry.jsx
@@ -6,8 +6,30 @@ import menu7 from '../../dist/assets/7.jpg';
 import menu31 from '../../dist/assets/31.jpg';
 import menu48 from '../../dist/assets/48.jpg';
 import menu41 from '../../dist/assets/41.jpg';
+import menu4 from '../../dist/assets/4.jpg';
+import menu27 from '../../dist/assets/27.jpg';
+import menu32 from '../../dist/assets/32.jpg';
+import menu33 from '../../dist/assets/33.jpg';
+import menu40 from '../../dist/assets/40.jpg';
+import menu45 from '../../dist/assets/45.jpg';
+import menu47 from '../../dist/assets/47.jpg';
+import menu54 from '../../dist/assets/54.jpg';
+import menu62 from '../../dist/assets/62.jpg';
+import menu63 from '../../dist/assets/63.jpg';
+import menu64 from '../../dist/assets/64.jpg';
 
 const imgs = {
+  64: menu64,
+  63: menu63,
+  62: menu62,
+  54: menu54,
+  47: menu47,
+  45: menu45,
+  40: menu40,
+  33: menu33,
+  32: menu32,
+  27: menu27,
+  4: menu4,
   41: menu41,
   48: menu48,
   31: menu31,
