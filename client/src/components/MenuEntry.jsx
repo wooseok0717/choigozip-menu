cd83ed827c261be33a5ec1eff437f965f7025211
Josh <wjang717@gmail.com>
2024-07-08 02:03:01 -0700

    adding images hard-coded

--- client/src/components/MenuEntry.jsx
+++ client/src/components/MenuEntry.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import {capitalize} from './helperfn';
+import menu2 from '../../dist/assets/2.jpg';
+import menu26 from '../../dist/assets/26.jpg';
+import menu7 from '../../dist/assets/7.jpg';
+import menu31 from '../../dist/assets/31.jpg';
+import menu48 from '../../dist/assets/48.jpg';
+import menu41 from '../../dist/assets/41.jpg';
+
+const imgs = {
+  41: menu41,
+  48: menu48,
+  31: menu31,
+  7: menu7,
+  26: menu26,
+  2: menu2
+}
 
 const MenuEntry = ({menu, lang}) => {
 
@@ -7,6 +22,8 @@ const MenuEntry = ({menu, lang}) => {
     <div className='menu-container'>
       <div className='img-container'>
         <img src={menu.img_url}/>
+        {/* {console.log(menu.id)} */}
+        <img src={imgs[menu.id]}/>
       </div>
       <div className='menu-name'>
         <span>
